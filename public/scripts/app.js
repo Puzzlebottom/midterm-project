7ff1840ab6253fb2616aac48daf85e2981ee7c6e
vagrant <hassan.a.issak@gmail.com>
2023-07-29 16:22:51 +0000

    Add code to create map marker on click

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -2,6 +2,7 @@
 /* eslint-disable no-undef */
 
 let map;
+let testVar;
 
 const initMap = async () => {
   const { Map } = await google.maps.importLibrary("maps");
@@ -9,7 +10,25 @@ const initMap = async () => {
   map = new Map(document.getElementById("map"), {
     center: { lat: -34.397, lng: 150.644 },
     zoom: 8,
+
   });
+  // Configure the click listener.
+  map.addListener("click", (mapsMouseEvent) => {
+    new google.maps.Marker({
+      position: mapsMouseEvent.latLng,
+      map,
+      title: "New marker on click!",
+    });
+    testVar = mapsMouseEvent.latLng;
+    console.log(mapsMouseEvent.latLng, '<==== MAP MARKER!')
+  });
+  new google.maps.Marker({
+    position: { lat: -34.397, lng: 150.644 },
+    map,
+    title: "Hello World!",
+  });
+
+  marker.setMap(map);
 
   console.log(map);
   return map;
@@ -19,4 +38,10 @@ $('#map-button').on('click', () => {
   initMap();
   console.log('MAP MEN');
 });
+
+
+
+
+
+
 
