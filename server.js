502aa813d0e468db6f733b540abd47abb31969a3
Conor Meldrum <conor.j.meldrum@gmail.com>
2023-07-29 15:16:41 -0400

    adds routing to new-game page, join-game page and bs-modal for help button

--- server.js
+++ server.js
@@ -31,11 +31,15 @@ app.use(express.static('public'));
 const userApiRoutes = require('./routes/users-api');
 const widgetApiRoutes = require('./routes/widgets-api');
 const usersRoutes = require('./routes/users');
+const newGameRoutes = require('./routes/new-game');
+const joinGameRoutes = require('./routes/join-game');
 
 // Mount all resource routes
 // Note: Feel free to replace the example routes below with your own
 // Note: Endpoints that return data (eg. JSON) usually start with `/api`
 app.use('/api/users', userApiRoutes);
+app.use('/new-game', newGameRoutes);
+app.use('/join-game', joinGameRoutes);
 app.use('/api/widgets', widgetApiRoutes);
 app.use('/users', usersRoutes);
 // Note: mount other resources here, using the same pattern above
