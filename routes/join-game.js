958b9d5d0ae963b7c6bba32caae32dfb6a4e53d5
Conor Meldrum <conor.j.meldrum@gmail.com>
2023-07-29 15:23:24 -0400

    adds apiKey as template var

--- routes/join-game.js
+++ routes/join-game.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  res.render('join-game');
+  res.render('join-game', { apiKey: process.env.API_KEY });
 });
 
 module.exports = router;
