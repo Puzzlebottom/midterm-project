6d15497e9a3e756e9faec6fc3a81a9d52e79ac96
Benson <bensonwlee@gmail.com>
2023-07-30 14:00:08 -0400

    fixed server.js file - was using previous version

--- server/cookies/cookie.js
+++ server/cookies/cookie.js
@@ -1,8 +1,23 @@
 const { v4: uuidv4 } = require('uuid');
+const db = require('../../db/connection')
 
 const checkCookie = function (request) {
-  const cookie = request.cookies.playerid;
+  const cookie = request.cookies.playerId;
   return cookie ? true : false;
-}
+};
 
-module.exports = {checkCookie};
+const giveCookie = function (response) {
+  return response.cookie('playerId', uuidv4(), { maxAge: 24 * 60 * 60 * 1000 , httpOnly: true }); // 24 hours
+};
+
+const getPlayerName = function (cookie) {
+  const queryString = `SELECT name FROM players WHERE cookie_uuid = $1`;
+  const values = [cookie.playerId];
+  db.query(queryString, values).then(
+  function(result) {
+    console.log(result);
+  }
+)
+};
+
+module.exports = {checkCookie, giveCookie, getPlayerName};
--- server/server.js
+++ server/server.js
@@ -1,25 +1,24 @@
 // load .env data into process.env
 require('dotenv').config();
 
-// import checkCookie.js
-const {checkCookie} = require('./cookies/cookie');
-
 // Web server config
 const sassMiddleware = require('../lib/sass-middleware');
+const cookieParser = require('cookie-parser')
 const express = require('express');
 const morgan = require('morgan');
-const cookieParser = require('cookie-parser');
 
-const PORT = process.env.PORT || 8080;
+const db = require('../db/connection.js');
+const PORT = 8080;
 const app = express();
+const {checkCookie, giveCookie, getPlayerName } = require('./cookies/cookie')
 
 app.set('view engine', 'ejs');
 
 // Load the logger first so all (static) HTTP requests are logged to STDOUT
 // 'dev' = Concise output colored by response status for development use.
 //         The :status token will be colored red for server error codes, yellow for client error codes, cyan for redirection codes, and uncolored for all other codes.
-app.use(cookieParser());
 app.use(morgan('dev'));
+app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
 app.use(
   '../styles',
@@ -31,6 +30,8 @@ app.use(
 );
 app.use(express.static('public'));
 
+// Separated Routes for each Resource
+// Note: Feel free to replace the example routes below with your own
 const newGameRoutes = require('./routes/new-game');
 const joinGameRoutes = require('./routes/join-game');
 
@@ -40,16 +41,36 @@ const joinGameRoutes = require('./routes/join-game');
 app.use('/new-game', newGameRoutes);
 app.use('/join-game', joinGameRoutes);
 
-// Note: mount other resources here, using the same pattern above
-
-// Home page
-// Warning: avoid creating more routes in this file!
-// Separate them into separate routes files (see above).
 
 app.get('/', (req, res) => {
   const hasCookie = checkCookie(req);
   console.log(hasCookie, '<==== COOKIE TRUE OR FALSE')
+  if (!hasCookie) {
+    giveCookie(res);
+  } else {
+    getPlayerName(cookie);
+  }
   res.render('index', { apiKey: process.env.API_KEY });
+});
+
+app.post('/saveMarker', (req, res) => {
+  const location = JSON.stringify(req.body);
+  console.log(location);
+
+  const queryString = `
+  INSERT INTO hiding_spots (location)
+  VALUES ($1)
+  RETURNING *;
+  `;
+
+  const queryValues = [location]; // Save the coordinates as a single string: "(lat, lng)"
+
+  return db.query(queryString, queryValues)
+    .then((result) => res.send(result.rows[0]))
+    .catch((error) => {
+      console.log("ERROR: ", error.message);
+      res.status(500).json({ error: "Error saving marker in the database." });
+    });
 });
 
 app.listen(PORT, () => {
