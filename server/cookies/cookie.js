e363ef05239099b90a1fbec4e52d92aefe916af9
Conor Meldrum <conor.j.meldrum@gmail.com>
2023-07-31 10:17:56 -0400

    updates cookie helpers to distinguish cookie types

--- server/cookies/cookie.js
+++ server/cookies/cookie.js
@@ -1,23 +1,34 @@
 const { v4: uuidv4 } = require('uuid');
-const db = require('../../db/connection')
+const db = require('../../db/connection');
 
-const checkCookie = function (request) {
-  const cookie = request.cookies.playerId;
-  return cookie ? true : false;
+const cookieParams = { maxAge: 24 * 60 * 60 * 1000, httpOnly: true }; //24 hours
+
+const checkPlayerCookie = (request) => {
+  const playerCookie = request.cookies.playerId;
+  return playerCookie ? true : false;
 };
 
-const giveCookie = function (response) {
-  return response.cookie('playerId', uuidv4(), { maxAge: 24 * 60 * 60 * 1000 , httpOnly: true }); // 24 hours
+const checkUserCookie = (request) => {
+  const userCookie = request.cookies.authorizedUser;
+  return userCookie ? true : false;
 };
 
-const getPlayerName = function (cookie) {
+const givePlayerCookie = (response) => {
+  return response.cookie('player', uuidv4(), cookieParams);
+};
+
+const giveUserCookie = (response) => {
+  return response.cookie('user', uuidv4(), cookieParams);
+};
+
+const getPlayerName = function(cookie) {
   const queryString = `SELECT screen_name FROM players WHERE cookie_uuid = $1`;
   const values = [cookie.playerId];
   db.query(queryString, values).then(
-  function(result) {
-    console.log(result);
-  }
-)
+    function(result) {
+      console.log(result);
+    }
+  );
 };
 
-module.exports = {checkCookie, giveCookie, getPlayerName};
+module.exports = { checkPlayerCookie, checkUserCookie, givePlayerCookie, giveUserCookie, getPlayerName };
