ca11190b9b480b8d69c4fa9617e3febc54fe6a8f
vagrant <hassan.a.issak@gmail.com>
2023-07-31 21:04:00 +0000

    Add query that returns lat and lng from table map_test and uses that to render maps api

--- server/routes/api/maps-api/map.js
+++ server/routes/api/maps-api/map.js
@@ -3,15 +3,26 @@
 
 const express = require('express');
 const router = express.Router();
+const db = require('../../../../db/connection');
 
-router.get('/', (req, res) => {
-  const templateVars = {
-    apiKey: process.env.API_KEY,
-    lat: 50,
-    lng: 10,
-    zoom: 8
-  }
-  res.render('map', templateVars);
+const queryString = `
+select * from map_test
+where id = 2`;
+const query = () => {
+  return db.query(queryString);
+};
+
+query().then((result) => {
+  console.log(result.rows[0]);
+  router.get('/', (req, res) => {
+    const templateVars = {
+      apiKey: process.env.API_KEY,
+      lat: result.rows[0].lat,
+      lng: result.rows[0].lng,
+      zoom: 8
+    }
+    res.render('map', templateVars);
+  });
 });
 
 module.exports = router;
