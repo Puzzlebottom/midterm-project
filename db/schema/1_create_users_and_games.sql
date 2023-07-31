e1b510f1d15e993f90244133e6665666d02719db
Conor Meldrum <conor.j.meldrum@gmail.com>
2023-07-31 16:38:50 -0400

    adds files for seeding db

--- db/schema/1_create_users_and_games.sql
+++ db/schema/1_create_users_and_games.sql
@@ -5,7 +5,7 @@ CREATE TABLE users(
   id SERIAL PRIMARY KEY NOT NULL,
   name VARCHAR(255) NOT NULL,
   password VARCHAR(255) NOT NULL,
-  cookie_uuid VARCHAR(255)
+  cookie_uuid UUID
 );
 
 CREATE TABLE games(
